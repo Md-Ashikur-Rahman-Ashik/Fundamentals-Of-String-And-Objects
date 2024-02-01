26f6f714c04f352eae81b43cfa707845981e8071
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-01 22:35:05 +0600

    Learning object done

--- declare.js
+++ declare.js
@@ -8,4 +8,33 @@ const pencil = new Object();
 // console.log(pencil);
 
 const rubber = Object.create({})
-console.log(rubber);
+// console.log(rubber);
+
+var person = {};
+person.name = "John";
+person.age = 30;
+// console.log(person);
+
+// let str = "Mississippi";
+// console.log(str.indexOf("i", 3));
+
+// const myObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+//   };
+  
+//   for (let prop in myObject) {
+//     console.log(myObject[prop]);
+//   }
+
+// const x = ""
+// if(!x.length){
+// console.log("Hey")
+// }else{
+// console.log("Hi")
+// }
+
+const str = "Hello";
+str[0] = "h";
+console.log(str);
