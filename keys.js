a55e1410e2ba8948b70deaf0e4a5c48ce99a8bef
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-01 15:13:44 +0600

    Learning keys in Javascript

--- keys.js
+++ keys.js
@@ -9,4 +9,23 @@ const computer = {
 const keys = Object.keys(computer);
 // console.log(keys);
 
-const values = Object.values()
+const values = Object.values(computer);
+// console.log(values);
+
+const college = {
+    name: "IPSC",
+    class: ["11", "12"],
+    events: ["Science fair", "Victory Day", "21st February"],
+    specialty: {
+        color: "Blue",
+        result: {
+            gpa: 5,
+            merit: "top",
+        }
+    }
+}
+
+// console.log(college.name);
+// console.log(college.specialty);
+// console.log(college.specialty.color);
+console.log(college);
