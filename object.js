f8c3b479b86fed2d285beea60e645e0f55f57453
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-01 09:06:16 +0600

    Learning object in Javascript

---
+++ object.js
@@ -0,0 +1,29 @@
+const age = 21;
+const school = "IPSC";
+const isPassed = true;
+let isDeveloper;
+const subjects = ["Bangla", "English", "Physics", "Math"];
+const wrongBottle = ["Water", 45];
+const bottleColor = "White";
+const bottlePrice = 45;
+const bottleDrinks = "Water";
+
+// console.log(isDeveloper);
+
+const bottle = {
+    brand: "Apple",
+    price: 45,
+    color: "White",
+    isClean: false
+}
+
+const subject = {
+    name: "Biology",
+    teacher: "Salma Mam",
+    examDate: "30 Feb",
+    chapters: ["First", "Second", "Third"],
+    exams: {
+        name: "Final Exam",
+        marks: 100
+    }
+}
