6fb0e563dd8bbd385a5062c16e490259a82886c6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-02 19:55:54 +0600

    Module finished

--- sort.js
+++ sort.js
@@ -4,6 +4,11 @@ const sortedPersons = persons.sort();
 // console.log(sortedPersons);
 
 const numbers = [4, 7, 2, 8, 3, 6];
-const numbers_asc = numbers.sort();
+// const numbers_asc = numbers.sort(function(a, b){
+//     return a - b;
+// });
+const numbers_dsc = [...numbers].sort(function(a, b){
+    return b - a;
+})
 
-console.log(numbers_asc);
+console.log(numbers_dsc);
